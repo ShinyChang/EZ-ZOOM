884b6646924033751aca1b87547fb8e7ca543715
ZhiHong Chang <ublue724@gmail.com>
2012-03-30 23:33:31 +0800

    Chrome Extension v1.2 Save the web zoom by localstorage

--- content.js
+++ content.js
@@ -1,32 +1,44 @@
 window.addEventListener("keydown", function(event) { 
+
 	var element;
 	if (event.target) {
 		element = event.target;
 	} else if (event.srcElement) {
-		element=event.srcElement;
+		element = event.srcElement;
 	}
 
 	if (element.nodeType == 3) {
-		element=element.parentNode;
+		element = element.parentNode;
 	}
-
+	
 	if (element.tagName == 'INPUT' || element.tagName == 'TEXTAREA') {
 		return;
 	}
-	var zoom = document.getElementsByTagName('html')[0].style.zoom;
-	zoom = zoom.substring(0, zoom.length - 1);
-	if (zoom == '') {
-		zoom = 100;
+
+	var zoom
+	if(localStorage.getItem("EZZOOM") != null) {
+		zoom = localStorage.getItem("EZZOOM");	
+	} else {
+		zoom = document.getElementsByTagName('html')[0].style.zoom;
+		zoom = zoom.substring(0, zoom.length - 1);
+		if (zoom == '') {
+			zoom = 100;
+		}
+		localStorage.setItem("EZZOOM", zoom);
 	}
+
 	if (event.keyCode == 109 ){// -
 		zoom -= 10;
 		if (zoom < 10) {
 			zoom = 10;
 		}
 		document.getElementsByTagName('html')[0].style.zoom = zoom + '%';
+		localStorage.setItem("EZZOOM", zoom);
 	}
 	else if (event.keyCode == 106 ){// *
+		zoom = 100;
 		document.getElementsByTagName('html')[0].style.zoom = '100%';
+		localStorage.setItem("EZZOOM", zoom);
 	}
 	else if (event.keyCode == 107 ){// +
 		zoom =  parseInt(zoom) +  parseInt(10);
@@ -34,5 +46,10 @@ window.addEventListener("keydown", function(event) {
 			zoom = 300;
 		}
 		document.getElementsByTagName('html')[0].style.zoom = zoom + '%';
+		localStorage.setItem("EZZOOM", zoom);
 	}
 });
+
+window.onload = function() {
+	document.getElementsByTagName('html')[0].style.zoom = localStorage.getItem("EZZOOM") + '%';
+};
