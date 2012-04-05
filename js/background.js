971e76aa642358e2eca9031fa01d10b186d83f73
ZhiHong Chang <ublue724@gmail.com>
2012-04-06 01:37:32 +0800

    Custom context menu enable/disable Add some annotation

--- js/background.js
+++ js/background.js
@@ -1,57 +1,96 @@
+// get current tab's domain
 function getCurrentTabDomain(callback) {
-    console.log("get current tab\'s domain");
-    chrome.tabs.getSelected(null, function(tab) {
-        callback(tab.url.match(/^[\w-]+:\/*\[?([\w\.:-]+)\]?(?::\d+)?/)[1]);
-    });
+	//console.log("get current tab\'s domain");
+	chrome.tabs.getSelected(null, function(tab) {
+		callback(tab.url.match(/^[\w-]+:\/*\[?([\w\.:-]+)\]?(?::\d+)?/)[1]);
+	});
 };
 
+// handle request which is from context or popup page
 function requestFromTabs(request, sender, sendResponse) {
+
+	//update database trigger by context or popup page
 	if(request.method == "setZoomLevel") {
-        chrome.browserAction.setBadgeText({
-            text : request.key
-        });
-        console.log("set zoom:" + request.key);
-        chrome.tabs.getSelected(null, function(tab) {
-            addDomainZoomLevel(tab.url.match(/^[\w-]+:\/*\[?([\w\.:-]+)\]?(?::\d+)?/)[1], request.key);
-        });
-        sendResponse({
-            status : "ok"
-        });
-    } else if(request.method == "getZoomLevel") {
-        chrome.tabs.getSelected(null, function(tab) {
-            ezZoom.indexedDB.getDomainZoomLevel(tab.url.match(/^[\w-]+:\/*\[?([\w\.:-]+)\]?(?::\d+)?/)[1], function(result) {
-                sendResponse({
-                    status : result
-                });
-            });
-        });
-    }
+
+		//update extension badge
+		chrome.browserAction.setBadgeText({
+			text : request.key
+		});
+		//console.log("set zoom:" + request.key);
+
+		//get current tab's domain and update database
+		chrome.tabs.getSelected(null, function(tab) {
+			addDomainZoomLevel(tab.url.match(/^[\w-]+:\/*\[?([\w\.:-]+)\]?(?::\d+)?/)[1], request.key);
+		});
+		//response
+		sendResponse({
+			status : "ok"
+		});
+
+		//response domain zoom level when tab switched, new tab created and popup page opened.
+	} else if(request.method == "getZoomLevel") {
+
+		//get current tab
+		chrome.tabs.getSelected(null, function(tab) {
+
+			//query database and response
+			ezZoom.indexedDB.getDomainZoomLevel(tab.url.match(/^[\w-]+:\/*\[?([\w\.:-]+)\]?(?::\d+)?/)[1], function(result) {
+
+				//response
+				sendResponse({
+					status : result
+				});
+			});
+		});
+	}
 };
 
+//set current tab's zoom level
 function setZoomOfCurrentTab(tabId, selectInfo) {
-    console.log("tab changed");
-    chrome.tabs.getSelected(null, function(tab) {
-        ezZoom.indexedDB.getDomainZoomLevel(tab.url.match(/^[\w-]+:\/*\[?([\w\.:-]+)\]?(?::\d+)?/)[1], function(result) {
-            console.log("tab zoom level:" + result);
+	// console.log("tab changed");
+
+	// get current tab
+	chrome.tabs.getSelected(null, function(tab) {
+		
+		//query domain zoom level
+		ezZoom.indexedDB.getDomainZoomLevel(tab.url.match(/^[\w-]+:\/*\[?([\w\.:-]+)\]?(?::\d+)?/)[1], function(result) {
+			//console.log("tab zoom level:" + result);
+			
+			//set default zoom level when current tab never zoomed or can not zoom 
 			if(result === undefined) {
 				result = "100";
 			}
-            chrome.browserAction.setBadgeText({
-                text : result
-            });
-        });
-    });	
-};
-
-function setDefaultZoomOnBadge(){
-	chrome.browserAction.setBadgeText({
-	    text : "100"
+			
+			//update extension badge
+			chrome.browserAction.setBadgeText({
+				text : result
+			});
+		});
 	});
 };
 
 
+//default zoom level badge
+function setDefaultZoomOnBadge() {
+	chrome.browserAction.setBadgeText({
+		text : "100"
+	});
+};
+
+//create context menu
+function createContextMenu() {
+	ezZoomContextMenu.create();
+};
+
+//remove context menu
+function destoryContextMenu() {
+	chrome.contextMenus.removeAll();
+}
+
+//init
 function initEzZoom() {
-	chrome.extension.onRequest.addListener(requestFromTabs);	
-	chrome.tabs.onActiveChanged.addListener(setZoomOfCurrentTab);	
+	chrome.extension.onRequest.addListener(requestFromTabs);
+	chrome.tabs.onActiveChanged.addListener(setZoomOfCurrentTab);
+	createContextMenu();
 	setDefaultZoomOnBadge();
 };
--- js/contextmenu.js
+++ js/contextmenu.js
@@ -1,15 +1,20 @@
-function genericOnClick(info, tab) {
-	chrome.tabs.sendRequest(tab.id, {
-		method : "zoomTarget"
-	}, function(response) {
-		// do nothing
-	});
-}
-
-function initContextMenu() {
-	var id = chrome.contextMenus.create({
-		"title" : "EZ ZOOM",
-		"contexts" : ["page", "selection", "link", "editable", "image", "video", "audio"],
-		"onclick" : genericOnClick
-	});
-}
+var ezZoomContextMenu = {
+	title : "EZ ZOOM",
+	create : function() {
+		this.destory();
+		chrome.contextMenus.create({
+			"title" : this.title,
+			"contexts" : ["page", "selection", "link", "editable", "image", "video", "audio"],
+			"onclick" : function(info, tab) {
+				chrome.tabs.sendRequest(tab.id, {
+					method : "zoomTarget"
+				}, function(response) {
+					// do nothing
+				});
+			}
+		});
+	},
+	destory : function() {
+		chrome.contextMenus.removeAll();
+	}
+};
